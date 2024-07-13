453144ea4841d9f343553cf9738b7cb8d1feaab5
Nialixus <nialixus@gmail.com>
2024-07-13 14:51:36 +0700

    [3.0.7] Update Test

--- test/typewriter_controller_test.dart
+++ test/typewriter_controller_test.dart
@@ -1,5 +1,43 @@
+import 'dart:async';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:typewritertext/typewritertext.dart';
+
+class ChatGPTStreamMock extends Stream<String> {
+  ChatGPTStreamMock({List<String>? message}) {
+    chat(message: message);
+  }
+
+  final _controller = StreamController<String>();
+
+  @override
+  StreamSubscription<String> listen(
+    void Function(String event)? onData, {
+    Function? onError,
+    void Function()? onDone,
+    bool? cancelOnError,
+  }) {
+    return _controller.stream.listen(
+      onData,
+      onError: onError,
+      onDone: onDone,
+      cancelOnError: cancelOnError,
+    );
+  }
+
+  Future<void> chat({List<String>? message}) async {
+    int index = 0;
+    List<String> response = message ?? ["Hello World"];
+
+    if (response.isNotEmpty) _controller.add(response[index]);
+    Timer.periodic(const Duration(milliseconds: 150), (_) {
+      if (index + 1 < response.length) {
+        index += 1;
+        _controller.add(response[index]);
+      }
+    });
+  }
+}
 
 void main() {
   group('TypeWriterController', () {
@@ -34,7 +72,7 @@ void main() {
               'World',
             ][controller.value.index]));
 
-        expect(controller.value.index <= 'Hello World'.length, isTrue);
+        expect(controller.value.index < 'Hello World'.length, isTrue);
       });
 
       await controller.start();
@@ -45,6 +83,49 @@ void main() {
       await controller.resume();
       expect(controller.value.index == 'Hello World'.length - 1, isTrue);
       expect(controller.value.text, equals('World'));
+    });
+  });
+
+  group('StreamController', () {
+    late TypeWriterController controller;
+
+    setUp(() {
+      controller = TypeWriterController.fromStream(ChatGPTStreamMock());
+    });
+
+    tearDown(() {
+      controller.dispose();
+    });
+
+    test('value', () async {
+      controller.addListener(() {
+        expect(
+            controller.value.text,
+            equals([
+              'H',
+              'He',
+              'Hel',
+              'Hell',
+              'Hello',
+              'Hello ',
+              'Hello W',
+              'Hello Wo',
+              'Hello Wor',
+              'Hello Worl',
+              'Hello World',
+            ][controller.value.index]));
+
+        expect(controller.value.index < 'Hello World'.length, isTrue);
+      });
+
+      await controller.start();
+
+      expect(controller.value.index == 'Hello World'.length - 1, isTrue);
+      expect(controller.value.text, equals('Hello World'));
+
+      await controller.resume();
+      expect(controller.value.index == 'Hello World'.length - 1, isTrue);
+      expect(controller.value.text, equals('Hello World'));
     });
   });
 }
--- test/typewriter_value_test.dart
+++ test/typewriter_value_test.dart
@@ -3,26 +3,37 @@ import 'package:typewritertext/typewritertext.dart';
 
 void main() {
   group('TypeWriterValue', () {
-    final value = TypeWriterValue(
-      ['the👑', 'hash', 'slinging', 'slasher'],
-      index: 3,
+    var value = TypeWriterValue(
+      ['the', 'hash', 'slinging', 'slasher'],
+      index: 2,
     );
 
     test('text', () {
-      expect(value.text, equals('the👑'));
-      value.index = 22;
-      expect(value.text, equals('slasher'));
-      value.index = 25;
       expect(value.text, equals('the'));
+      value.index = 21;
+      expect(value.text, equals('slasher'));
+      value.index = 24;
+      expect(value.text, equals('the'));
+      expect(value.length, equals(22));
       expect(
           value.toString(),
           equals('TypeWriterValue('
-              'data: [the👑, hash, slinging, slasher], '
+              'data: [the, hash, slinging, slasher], '
               'text: the, '
               'index: 2)'));
+      value += ' 👑';
+      expect(value.text, equals('thehashslingingslasher 👑'));
+      expect(value.length, equals(24));
+      expect(value.index, equals(23));
+      expect(
+          value.toString(),
+          equals('TypeWriterValue('
+              'data: [thehashslingingslasher 👑], '
+              'text: thehashslingingslasher 👑, '
+              'index: 23)'));
     });
     test('length', () {
-      expect(value.length, equals(23));
+      expect(value.length, equals(24));
       value.data = ['dummy'];
       expect(value.length, equals(5));
     });
